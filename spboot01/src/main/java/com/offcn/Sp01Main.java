96bbcdd166642d0f400da0266214a83aa9b3a97a
jiabao <11348865@qq.com>
2020-07-24 15:52:01 +0800

    V1.0.2 delet 无用的类

--- spboot01/src/main/java/com/offcn/Sp01Main.java
+++ spboot01/src/main/java/com/offcn/Sp01Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sp01Main {
     //1.0.1
+
+    //三天
     public static void main(String[] args) {
         SpringApplication.run(Sp01Main.class,args);
     }
